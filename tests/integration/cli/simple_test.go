31b5966906ae1299d4ecb318c81ed412b181871d
Andrew Sisley <a.j.sisley@hotmail.com>
2022-11-28 16:40:03 -0500

    Add nil tests

--- tests/integration/cli/simple_test.go
+++ tests/integration/cli/simple_test.go
@@ -59,3 +59,99 @@ func TestSimple(t *testing.T) {
 		},
 	)
 }
+
+func TestSimpleWithEmptyItem(t *testing.T) {
+	type Input struct {
+		Name string
+		Age  int
+	}
+
+	type Output struct {
+		FullName string
+		Age      int
+	}
+
+	executeTest(
+		t,
+		TestCase[Input, Output]{
+			LensFile: `
+			{
+				"lenses": [
+					{
+						"path": "` + modules.WasmPath1 + `"
+					}
+				]
+			}`,
+			Input: []Input{
+				{
+					Name: "John",
+					Age:  3,
+				},
+				{},
+				{
+					Name: "Orpheus",
+					Age:  7,
+				},
+			},
+			ExpectedOutput: []Output{
+				{
+					FullName: "John",
+					Age:      3,
+				},
+				{},
+				{
+					FullName: "Orpheus",
+					Age:      7,
+				},
+			},
+		},
+	)
+}
+
+func TestSimpleWithNilItem(t *testing.T) {
+	type Input struct {
+		Name string
+		Age  int
+	}
+
+	type Output struct {
+		FullName string
+		Age      int
+	}
+
+	executeTest(
+		t,
+		TestCase[*Input, *Output]{
+			LensFile: `
+			{
+				"lenses": [
+					{
+						"path": "` + modules.WasmPath1 + `"
+					}
+				]
+			}`,
+			Input: []*Input{
+				{
+					Name: "John",
+					Age:  3,
+				},
+				nil,
+				{
+					Name: "Orpheus",
+					Age:  7,
+				},
+			},
+			ExpectedOutput: []*Output{
+				{
+					FullName: "John",
+					Age:      3,
+				},
+				nil,
+				{
+					FullName: "Orpheus",
+					Age:      7,
+				},
+			},
+		},
+	)
+}
