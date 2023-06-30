acdc3794600b8a70a8fc891901be46de1b9037a2
AndrewSisley <a.j.sisley@hotmail.com>
2023-06-30 07:06:46 -0400

    Add MPL License (#48)
    * Add MPL License
    
    Temporary, until lawyers get back to us with long-term license.
    
    * Add file license headers

--- sdk-rust/src/lib.rs
+++ sdk-rust/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 /*!
 This crate contains members to aid in the construction of a Rust [Lens Module](https://github.com/lens-vm/spec#abi---wasm-module-functions).
 */
